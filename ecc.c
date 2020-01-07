1d3ea3f3559d0d4ab9dc705627708e063d4e62de
kawa g3 <kawag3@gmail.com>
2020-01-07 03:28:34 -0800

    accept -1, +2, -(1+2)

--- ecc.c
+++ ecc.c
@@ -48,6 +48,7 @@ struct Node {
 
 Node *expr();
 Node *mul();
+Node *unary();
 Node *primary();
 
 // 入力プログラム
@@ -189,18 +190,25 @@ Node *expr() {
 
 
 Node *mul() {
-  Node *node = primary();
+  Node *node = unary();
 
   for (;;) {
     if (consume('*'))
-      node = new_node(ND_MUL, node, primary());
+      node = new_node(ND_MUL, node, unary());
     else if (consume('/'))
-      node = new_node(ND_DIV, node, primary());
+      node = new_node(ND_DIV, node, unary());
     else
       return node;
   }
 }
 
+Node *unary() {
+  if (consume('+'))
+    return primary();
+  if (consume('-'))
+    return new_node(ND_SUB, new_node_num(0), primary());
+  return primary();
+}
 
 Node *primary() {
   // 次のトークンが"("なら、"(" expr ")"のはず
